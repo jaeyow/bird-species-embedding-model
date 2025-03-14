453d45a9dac2b81f2b9ef397c7e960a8735401a9
jaeyow <jaeyow@gmail.com>
2025-03-15 08:28:32 +1100

    Pydantic response

--- fastapi/app/api/endpoints/router.py
+++ fastapi/app/api/endpoints/router.py
@@ -1,4 +1,7 @@
 import json
+from typing import List
+
+from pydantic import BaseModel
 from fastapi import APIRouter, File, UploadFile
 
 from app.services.generator import EmbeddingGeneratorService
@@ -6,11 +9,16 @@ import lancedb
 
 router = APIRouter()
 
+class BirdSimilarityResult(BaseModel):
+    image_path: str
+    label: str
+    similarity: float
+    
 @router.post(
     "/get_similar_birds",
     tags=["get_similar_birds"],
     summary="Get similar birds")
-async def get_similar_birds(file: UploadFile = File(...), limit: int=6, metric: str="cosine"):
+async def get_similar_birds(file: UploadFile = File(...), limit: int=6, metric: str="cosine") -> List[BirdSimilarityResult]:
     content = await file.read()
     embedding_service = EmbeddingGeneratorService()
     unknown_bird = embedding_service.generate_embedding_from_bytes(content)
@@ -29,7 +37,7 @@ async def get_similar_birds(file: UploadFile = File(...), limit: int=6, metric: 
     results["similarity"] = 1 - results["_distance"]
     results = results.drop(columns=["_distance"]) # Drop the distance column, just need the similarity
 
-    results_list = results.to_dict(orient="records")
+    results_list = [BirdSimilarityResult(**result) for result in results.to_dict(orient="records")]
 
     return results_list
 
